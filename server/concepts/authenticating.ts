7bb635742738f082fada2aa49f3c19eb4888ddea
Nisha <nishabk@mit.edu>
2024-09-25 23:29:25 -0400

    implemented prep 4

--- server/concepts/authenticating.ts
+++ server/concepts/authenticating.ts
@@ -36,7 +36,7 @@ export default class AuthenticatingConcept {
     // TODO 1: implement this operation
     //  - use this.users.readOne(..)
     //  - don't include the password (we've provided a helper function you can use!)
-    throw new Error("Not implemented!");
+    return await this.users.readOne({ _id });
   }
 
   async getUsers(username?: string) {
@@ -58,7 +58,9 @@ export default class AuthenticatingConcept {
     // TODO 2: implement this operation
     //  - use this.users.partialUpdateOne(..)
     //  - maintain the invariant that usernames are unique (we've provided a helper function!)
-    throw new Error("Not implemented!");
+    await this.assertUsernameUnique(username);
+    await this.users.partialUpdateOne({ _id }, { username });
+    return { msg: "Username updated successfully!", user: await this.users.readOne({ _id }) };
   }
 
   async delete(_id: ObjectId) {
